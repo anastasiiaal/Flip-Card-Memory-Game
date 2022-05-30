58fbcc09ceff7acbe9ba9fda4b0462b9546d57cc
anastasiiaal <anastasia.alexeenko@gmail.com>
2022-05-30 16:52:19 +0200

    :moon: minor corrections

--- connect.php
+++ connect.php
@@ -1,5 +1,12 @@
 <?php
-try {
+// try {   // connection for the online platform
+//         $db = new PDO('mysql:host=localhost;dbname=anastasiiaa_memory-game;charset=utf8', 'anastasiiaa', 'lkXSqpFWqAPE3Q==');
+//     } catch(Exception $e){
+//         echo 'Erreur : ' . $e->getMessage();
+//     }
+
+
+try {    // connection for the wamp localhost
         $db = new PDO('mysql:host=localhost;dbname=flip-card-game;charset=utf8', 'root');
     } catch(Exception $e){
         echo 'Erreur : ' . $e->getMessage();
